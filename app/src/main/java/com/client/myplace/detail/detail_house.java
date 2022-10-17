a18beac1f5e7a238d9cdf8be83211b109be8e4d5
tanaykulkarni27 <1002kulkarnitp@gmail.com>
2022-10-17 08:42:03 +0530

    Updated app

---
+++ app/src/main/java/com/client/myplace/detail/detail_house.java
@@ -0,0 +1,16 @@
+package com.client.myplace.detail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.client.myplace.R;
+
+public class detail_house extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_house);
+    }
+}
